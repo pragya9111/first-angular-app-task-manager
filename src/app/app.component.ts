7281af7076855bf7be954066387792552f9ef580
Pragya Bisen <pragyabisen14@gmail.com>
2025-03-05 16:43:38 +0530

    using @for and @if for rendering dynamic content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { TaskComponent } from './task/task.component';
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
