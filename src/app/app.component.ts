e7a3dfccf65cefbc897e31cdf3ae9f0d9bcd7308
Pragya Bisen <pragyabisen14@gmail.com>
2025-03-05 16:01:53 +0530

    added task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,24 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
 
-   onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
   }
 }
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks',
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+  @Input({ required: true }) name!: string;
+}
