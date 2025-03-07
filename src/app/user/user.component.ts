1472005f702ae176499dabfd2b396ee01ac534c2
Pragya Bisen <pragyabisen14@gmail.com>
2025-03-07 14:19:25 +0530

    created common ui cmp and used date pipe

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,10 @@
 import { Component, computed, EventEmitter, Input, input, output, Output } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
 import { type User } from './user.model';
+import { CardComponent } from '../ui/card/card.component';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
