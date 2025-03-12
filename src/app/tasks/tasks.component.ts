51c93b5f9088351f28c2f9c6dec60d3981ce7abc
Pragya Bisen <pragyabisen14@gmail.com>
2025-03-12 07:46:16 +0530

    added basic task service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { NewTaskComponent } from './new-tasks/new-task.component';
 import { type NewTaskData } from './task/task.model';
+import { TaskServices } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -14,38 +15,23 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
   isAddTaskVisible = false;
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  
+  // private tasksService: TaskServices;
+
+  // //Dependency Injection- here you tell Angular which type of value you need and Angular creates it and provided it as an argument. 
+  // constructor(tasksService: TaskServices) {
+  //   this.tasksService = tasksService;
+  // }
+
+  // This is a shorthand way of defining a constructor and initializing the tasksService property.
+  constructor(private tasksService: TaskServices) { }
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId);
+
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
   }
 
   onAddTaskDialog() {
@@ -57,13 +43,6 @@ export class TasksComponent {
   }
 
   onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    })
     this.isAddTaskVisible = false;
   }
 }
